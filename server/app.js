ecc4c3982fbeae3f8c113c1447f9fa0423f8aec2
MakomaneTau <151774523+MakomaneTau@users.noreply.github.com>
2024-11-01 11:35:07 +0200

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname , 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/about.html'));
+  });
+  
+  app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/product.html'));
+  });
+  
+  app.get('/shop', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/shop.html'));
+  });
+
+  app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/cart.html'));
+  });
+
+
+app.use((req, res) =>{
+    res.status(404);
+    res.send('<h1>Error 404:Resource not found</h1>');
+})
+
+app.listen(3000, ()=>{
+    console.group("App listening on port 3000");
+})
